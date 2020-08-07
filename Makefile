61efac88f41e27385bd4b62de8f5a131d7df2488
Hoang Nguyen <hoang.nguyen.11084@gmail.com>
2020-08-06 23:07:23 -0700

    make target to run postgres

--- Makefile
+++ Makefile
@@ -17,9 +17,27 @@ build: vendor
 		-o dist/${APP_NAME} \
 		-ldflags "-s -X main.version=$(VERSION) -X main.revision=$(REVISION)"
 
+.PHONY: run.db
+run.db:
+	docker run --rm \
+		--name pg-docker \
+		-e POSTGRES_PASSWORD=postgres \
+		-p 127.0.0.1:5432:5432 \
+		-v ~/docker/volumes/postgres:/var/lib/postgresql/data \
+		-d postgres
+	docker run --rm \
+		--name pd-admin \
+		-p 80:80 \
+		-v ~/docker/volumes/pg-admin:/var/lib/pgadmin \
+		-v ~/docker/volumes/pg-servers.json:/pgadmin4/servers.json \
+		-e 'PGADMIN_DEFAULT_EMAIL=admin@gocart.com' \
+		-e 'PGADMIN_DEFAULT_PASSWORD=admin' \
+		-d dpage/pgadmin4
+
 .PHONY: run
 run: build
 	godotenv ./dist/${APP_NAME}
+
 .PHONY: test
 test:
 	go test ./... \
--- public/js/addItem.js
+++ public/js/addItem.js
@@ -1,18 +1,56 @@
+function getSelector(node) {
+    var id = node.getAttribute('id');
+    if (id) {
+        return '#'+id;
+    }
+    var path = '';
+    while (node) {
+        var name = node.localName;
+        var parent = node.parentNode;
+        if (!parent) {
+            path = name + ' > ' + path;
+            continue;
+        }
+        if (node.getAttribute('id')) {
+            path = '#' + node.getAttribute('id') + ' > ' + path;
+            break;
+        }
+        var sameTagSiblings = [];
+        var children = parent.childNodes;
+        children = Array.prototype.slice.call(children);
+        children.forEach(function(child) {
+            if (child.localName == name) {
+                sameTagSiblings.push(child);
+            }
+        });
+        // if there are more than one children of that type use nth-of-type
+        if (sameTagSiblings.length > 1) {
+            var index = sameTagSiblings.indexOf(node);
+            name += ':nth-of-type(' + (index + 1) + ')';
+        }
+        if (path) {
+            path = name + ' > ' + path;
+        } else {
+            path = name;
+        }
+        node = parent;
+    }
+    return path;
+}
+
 function setupIframeJS (e) {
     iframeDoc = e.srcElement.contentDocument
     iframeDoc.addEventListener("click", function (iframeClickEvent) {
         x = iframeClickEvent.clientX
         y = iframeClickEvent.clientY
         clickedEL = iframeDoc.elementFromPoint(x, y)
-        console.log("----- value = ", clickedEL.innerText)
+        console.log("----- value = ", clickedEL.innerText, getSelector(clickedEL))
     })
 }
 
 function onItemAddSuccess (rsp) {  
     const previewURL = rsp.data.url
-
     const iframe = document.getElementById("preview")
-
     iframe.addEventListener("load", setupIframeJS)
     document.getElementById("preview").src=previewURL
 }
@@ -28,7 +66,6 @@ function loadURL (e) {
         alert("Please enter a URL")
         return
     }
-
     axios({
         method: 'post',
         url: '/api/items',
