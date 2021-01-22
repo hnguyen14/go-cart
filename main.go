98f7d0cd9b5f353161c2c7281917c2a1fda61106
Hoang Nguyen <hoang.nguyen.11084@gmail.com>
2021-01-21 17:07:48 -0800

    Excessive log

--- main.go
+++ main.go
@@ -10,5 +10,12 @@ func main() {
 	a := app.NewApp("./config.yml")
 	server := a.NewServer()
 
+	fmt.Sprintf("Starting Go Server now")
+	fmt.Sprintf("Starting Go Server now")
+	fmt.Sprintf("Starting Go Server now")
+	fmt.Sprintf("Starting Go Server now")
+	fmt.Sprintf("Starting Go Server now")
+	fmt.Sprintf("Starting Go Server now")
+	fmt.Sprintf("Starting Go Server now")
 	server.Run(fmt.Sprintf(":%v", a.Config.Server.Port))
 }
